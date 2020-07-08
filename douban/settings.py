20d37041ec7c3684f500fa39bbbc3721f5f3ff47
叶凤 <feng.ye@qcast.cn>
2020-07-08 10:10:18 +0800

    user agent

--- douban/settings.py
+++ douban/settings.py
@@ -52,9 +52,11 @@ ROBOTSTXT_OBEY = True
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
+DOWNLOADER_MIDDLEWARES = {
 #    'douban.middlewares.DoubanDownloaderMiddleware': 543,
-#}
+#    'douban.middlewares.my_proxy': 543,
+   'douban.middlewares.my_useragent': 543,
+}
 
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
